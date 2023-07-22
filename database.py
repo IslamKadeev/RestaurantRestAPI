ad3740abd02eb2ead95ab45553f15a10ee22f394
IslamKadeev <kadeevkgeu@gmail.com>
2023-07-22 22:57:47 +0300

    Create db default configuration

--- database.py
+++ database.py
@@ -1,13 +1,25 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from settings import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:8000/db"
+SQLALCHEMY_DATABASE_URL = settings.database_url
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autocommit=False,
+    autoflush=False
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
