1f684f6990c1b71eee09a4fd3b223b1453d59f3b
IslamKadeev <kadeevkgeu@gmail.com>
2023-07-24 20:52:37 +0300

    Edited schemas

---
+++ schema.py
@@ -0,0 +1,56 @@
+from pydantic import BaseModel
+from typing import List
+
+
+class DishBase(BaseModel):
+    title: str
+    description: str
+    price: float
+
+
+class DishCreate(DishBase):
+    pass
+
+
+class Dish(DishBase):
+    id: str
+
+    class Config:
+        orm_mode = True
+
+
+class SubmenuBase(BaseModel):
+    title: str
+    description: str
+
+
+class SubmenuCreate(SubmenuBase):
+    pass
+
+
+class Submenu(SubmenuBase):
+    id: str
+    menu_id: str
+    dishes: List[Dish] = []
+    dish_count: int
+
+    class Config:
+        orm_mode = True
+
+
+class MenuBase(BaseModel):
+    title: str
+    description: str
+
+
+class MenuCreate(MenuBase):
+    pass
+
+
+class Menu(MenuBase):
+    id: str
+    submenus: List[Submenu] = []
+    submenu_count: int 
+
+    class Config:
+        orm_mode = True
